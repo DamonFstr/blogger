67903ec7aa4544d29350ca62d35b5d10c6af00eb
Damon Foster <damon.fstr@gmail.com>
2017-02-12 00:16:52 +0000

    Sorcery authentication complete

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ end
 
 gem "paperclip"
 gem "intercom-rails"
+gem 'sorcery'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,14 +1,15 @@
 class TagsController < ApplicationController
+before_filter :require_login, only: [:destroy]
 
   def show
-    @tag = Tag.find(params[:id])
-  end
+  @tag = Tag.find(params[:id])
+end
 
-  def index
+def index
     @tags = Tag.all
   end
 
-  def destroy
+def destroy
     @tag = Tag.find params[:id]
     @tag.destroy
     flash.notice = "Tag '#{@tag.name}' Deleted!"
